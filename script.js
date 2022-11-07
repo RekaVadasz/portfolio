faac361c6712d11fe64055097fa84558f1dd5dd1
RekaVadasz <vadasz.reka87@gmail.com>
2022-11-07 16:58:02 +0100

    Add Skills, Contact and media queries

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+const menu = document.querySelector("#menu-icon");
+const navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navlist.classList.toggle("active");
+}
+
+window.onscroll = () => {
+    menu.classList.remove("bx-x");
+    navlist.classList.remove("active");
+}
+
